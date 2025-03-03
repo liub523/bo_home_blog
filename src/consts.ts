3ec381310fe560e8050ff6fa670cfd66b330653b
liub523 <443008107@qq.com>
2025-03-03 15:22:11 +0800

    change theme to astro zen

--- src/consts.ts
+++
@@ -1,5 +0,0 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = "我的个人博客";
-export const SITE_DESCRIPTION = "欢迎来到我的个人网站，这里记录着我的思考与成长。";
